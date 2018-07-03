c7f4e818735de9e0e79dc21c998d90a38c2b236e
Batuhan Kından <batuhan.kindan@gmail.com>
2018-07-03 15:47:14 +0300

    STLExport.cs file modified
    Get mesh vertices and get mesh normals functions defined inside the class.

--- triangle_mesh 1/STL_Tools/STLExport.cs
+++ triangle_mesh 1/STL_Tools/STLExport.cs
@@ -26,6 +26,60 @@ namespace STL_Tools
     public class STLExport
     {
 
+        /**
+        * @brief  This function creates a vertice array for OpenGL vertex array object from gşven triangular mesh array
+        * @param  meshArray
+        * @retval vertices
+        */
+        public float[] Get_Mesh_Vertices(TriangleMesh[] meshArray)
+        {
+            List<float> vertices = new List<float>();
+
+            int i = 0;
+
+            for (i = 0; i < meshArray.Length; i++)
+            {
+                /* vertex 1 */
+                vertices.Add(meshArray[i].vert1.x);
+                vertices.Add(meshArray[i].vert1.y);
+                vertices.Add(meshArray[i].vert1.z);
+                /* vertex 2 */
+                vertices.Add(meshArray[i].vert2.x);
+                vertices.Add(meshArray[i].vert2.y);
+                vertices.Add(meshArray[i].vert2.z);
+                /* vertex 3 */
+                vertices.Add(meshArray[i].vert3.x);
+                vertices.Add(meshArray[i].vert3.y);
+                vertices.Add(meshArray[i].vert3.z);
+            }
+
+                return vertices.ToArray();
+        }
+
+
+        /**
+        * @brief  This function creates a normal array for OpenGL vertex array object from given triangular mesh array
+        * @param  meshArray
+        * @retval normals
+        */
+        public float[] Get_Mesh_Normals(TriangleMesh[] meshArray)
+        {
+            List<float> normals = new List<float>();
+
+            int i = 0;
+
+            for (i = 0; i < meshArray.Length; i++)
+            {
+                /* vertex 1 */
+                normals.Add(meshArray[i].normal.x);
+                normals.Add(meshArray[i].normal.y);
+                normals.Add(meshArray[i].normal.z);
+            }
+
+            return normals.ToArray();
+        }
+
+
     }
 
 }
