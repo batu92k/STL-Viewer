61f4c3057bba5d4cc94448926b54abee2ae82c9e
Batuhan Kından <batuhan.kindan@gmail.com>
2018-07-03 15:46:26 +0300

    STLExport.cs source file created

---
+++ triangle_mesh 1/STL_Tools/STLExport.cs
@@ -0,0 +1,31 @@
+﻿/**
+  ******************************************************************************
+  * @file    STLExport.cs
+  * @author  Ali Batuhan KINDAN
+  * @version V1.0.0
+  * @date    03.07.2018
+  * @brief   
+  ******************************************************************************
+  */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+using System.Threading;
+
+namespace STL_Tools
+{
+
+    public class STLExport
+    {
+
+    }
+
+}
