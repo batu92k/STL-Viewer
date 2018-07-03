5bb9a406f154361fa53036c3d8e96184314464db
Batuhan Kından <batuhan.kindan@gmail.com>
2018-07-03 12:35:27 +0300

    TriangleMesh.cs file created
    The source file which contains triangluar mesh property class added to project

---
+++ triangle_mesh 1/STL_Tools/TriangleMesh.cs
@@ -0,0 +1,40 @@
+﻿/**
+  ******************************************************************************
+  * @file    TriangleMesh.cs
+  * @author  Ali Batuhan KINDAN
+  * @version V1.0.0
+  * @date    03.07.2018
+  * @brief   
+  ******************************************************************************
+  */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+
+namespace STL_Tools
+{
+    public class TriangleMesh
+    {
+        public Vector3 normal;
+        public Vector3 vert1;
+        public Vector3 vert2;
+        public Vector3 vert3;
+
+        /**
+        * @brief  Class instance constructor
+        * @param  none
+        * @retval none
+        */
+        public TriangleMesh()
+        {
+            normal = new Vector3();
+            vert1 = new Vector3();
+            vert2 = new Vector3();
+            vert3 = new Vector3();
+        }
+
+
+    }
+
+}
