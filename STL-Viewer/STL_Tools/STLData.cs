669df2ee36327e53dac7043ba0e351f3ca797ca0
Batuhan Kından <batuhan.kindan@gmail.com>
2022-12-27 00:37:39 +0100

    No need for constructor in StlData class

--- STL-Viewer/STL_Tools/STLData.cs
+++ STL-Viewer/STL_Tools/STLData.cs
@@ -12,17 +12,9 @@ namespace STL_Tools
 {
     public class StlData
     {
-        public List<TriangleMesh> meshList;
+        public List<TriangleMesh> meshList = new List<TriangleMesh>();
         public Vector3 maxVec = new Vector3(); // max position of model
         public Vector3 minVec = new Vector3(); // min position of model
         public StlFileType fileType = StlFileType.NONE;
-
-        /// <summary>
-        /// Default constructor of STLData class.
-        /// </summary>
-        public StlData()
-        {
-            meshList = new List<TriangleMesh>();
-        }
     }
 }
